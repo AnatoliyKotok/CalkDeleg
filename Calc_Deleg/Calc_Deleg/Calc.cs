071cb1b677b2a7cfa114c95ec4cbe616dd277e11
AnatoliyKotok <61416716+AnatoliyKotok@users.noreply.github.com>
2020-10-21 11:56:11 +0300

    Add files via upload

---
+++ Calc_Deleg/Calc_Deleg/Calc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc_Deleg
+{
+    class Calc
+    {
+        Func<double, double, double> func;
+        public double Plus(double a,double b)
+        {
+            return a + b;
+        }
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        public double Mult(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            return a /b;
+        }
+        public void Calculator(char s,double a,double b)
+        {
+            switch (s)
+            {
+                case '+':
+                    func = Plus;
+                    Console.WriteLine( func(a,b));
+                    break;
+                case '-':
+                    func = Minus;
+                    Console.WriteLine( func(a,b));
+                    break;
+                case '*':
+                    func = Mult;
+                    Console.WriteLine(func(a,b));
+                    break;
+                case '/':
+                    func = Division;
+                    Console.WriteLine(func(a,b));
+                    break;
+            }
+        }
+    }
+}
---
+++ Calc_Deleg/Calc_Deleg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calc_Deleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            calc.Calculator('+', 3, 5);
+        }
+    }
+}
